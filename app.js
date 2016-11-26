dfb8eca51371667d34cf4d40d2a27a1c2bc49f86
Suhzi <blesmips@yahoo.com>
2016-11-26 00:08:42 +0000

    Comments included in app.js and index.html

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-(function (){
-  'use strict';
+(function () {
+	'use strict';
 
-  angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
+	angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
 
-  LunchCheckController.$inject = ['$scope'];
+	LunchCheckController.$inject = ['$scope'];
 
-  function LunchCheckController($scope) {
-    $scope.lunchItems="";
-    $scope.message;
+	function LunchCheckController($scope) {
+		$scope.lunchItems = "";
+		$scope.message;
 
-  $scope.checkStatus = function(){
+		$scope.checkStatus = function () {
 
-    if ($scope.lunchItems === "" || $scope.lunchItems === " "){
-      $scope.message = "Please enter data first";
-    } else {
-      var lunchItemsArray = $scope.lunchItems.split(',');
-      if(lunchItemsArray.length <= 3){
-        $scope.message = "Enjoy!";
-      }else{
-        $scope.message = "Too much";
-      }
-    }
-    return $scope.message;
-   }//end of checkStatus function
+				//check if nothing is entered
+				if ($scope.lunchItems === "" || $scope.lunchItems === " ") {
+					$scope.message = "Please enter data first";
+				} else {
+					var lunchItemsArray = $scope.lunchItems.split(','); //the split method is used
 
- }//end of LunchCheckController function
+					//check the number of items
+					if (lunchItemsArray.length <= 3) {
+						$scope.message = "Enjoy!";
+					} else {
+						$scope.message = "Too much";
+					}
+				}
+				return $scope.message;
+			} //end of checkStatus function
+
+	} //end of LunchCheckController function
 
 })();
